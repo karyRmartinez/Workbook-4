3119ba2d49c823b52f7a0feece804d2f17f3cd08
karyRmartinez <karymartinez@pursuit.org>
2023-11-08 16:08:20 -0500

    Contract class completed

---
+++ dealership-inventory/src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+public abstract class Contract {
+
+    private String contractDate;
+    private String customerName;
+    private String customerEmail;
+    private String vehicleSold;
+    public Contract(String contractDate, String customerName, String customerEmail, String vehicleSold) {
+        this.contractDate = contractDate;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+//   private double getTotalPrice(){
+//        return 0;
+//    }
+   public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
+    //double getMonthlyPayment();
+
+    // Getters and setters
+    public String getContractDate() {
+        return contractDate;
+    }
+
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+}
